72c7619fd8eeec964368720dd815cf286917d8da
piomar123 <piomar123@gmail.com>
2016-10-24 18:56:58 +0200

    Task 1 done. Direct POST files using form. Adding metadata of uploader hostname.

--- actions/createform.js
+++ actions/createform.js
@@ -5,12 +5,13 @@ var S3Form = require("../s3post").S3Form;
 var AWS_CONFIG_FILE = "config.json";
 var POLICY_FILE = "policy.json";
 var INDEX_TEMPLATE = "index.ejs";
-
+var os = require("os");
 
 var task = function(request, callback){
 	//1. load configuration
 	var awsConfig = helpers.readJSONFile(AWS_CONFIG_FILE);
 	var policyData = helpers.readJSONFile(POLICY_FILE);
+	var hiddenFields = [];
 
 	//2. prepare policy
 	var policy = new Policy(policyData);
@@ -18,9 +19,14 @@ var task = function(request, callback){
 	//3. generate form fields for S3 POST
 	var s3Form = new S3Form(policy);
 	//4. get bucket name
-	
 
-	callback(null, {template: INDEX_TEMPLATE, params:{fields:[], bucket:""}});
+	policy.getConditions().push({ "x-amz-meta-uploader": os.hostname() });
+	hiddenFields = s3Form.generateS3FormFields();
+	hiddenFields = s3Form.addS3CredientalsFields(hiddenFields, awsConfig);
+
+	callback(null, {template: INDEX_TEMPLATE, params:{
+		fields:hiddenFields, bucket:policy.getConditionValueByKey("bucket")
+	}});
 }
 
 exports.action = task;
--- actions/s3digest.js
+++ actions/s3digest.js
@@ -3,9 +3,7 @@ var AWS = require("aws-sdk");
 AWS.config.loadFromPath('./config.json');
 
 var task =  function(request, callback){
-
-	
-	
+	callback(null, "File " + request.query.key + " saved in bucket " + request.query.bucket);
 }
 
 exports.action = task
