9706060c7165ec7425b96bb0bcd42cb44cc99ad3
piomar123 <piomar123@gmail.com>
2016-10-24 19:26:47 +0200

    Task 2 done. Calculating digests of uploaded file.

--- actions/s3digest.js
+++ actions/s3digest.js
@@ -1,9 +1,31 @@
 var AWS = require("aws-sdk");
+var helpers = require("../helpers.js");
 
 AWS.config.loadFromPath('./config.json');
 
-var task =  function(request, callback){
-	callback(null, "File " + request.query.key + " saved in bucket " + request.query.bucket);
+var s3 = new AWS.S3();
+
+var task =  function(request, next){
+	var reqParams = {
+		Bucket: request.query.bucket,
+		Key: request.query.key,
+	};
+
+	var showDigests = function(err, digests) {
+		next(null, digests.join("<br>") + "<hr>");
+	};
+
+	var calcDigests = function(err, data){
+		if(err) return next(err);
+
+		helpers.calculateMultiDigest(
+			data.Body,
+			['md5', 'sha1', 'sha256', 'sha512'],
+			showDigests, 1);
+	};
+
+	s3.getObject(reqParams, calcDigests);
+	//callback(null, "File " + request.query.key + " saved in bucket " + request.query.bucket);
 }
 
 exports.action = task
