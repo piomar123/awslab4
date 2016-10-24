d4809f92b90b30dff5b43fc9aa626c7d55d98cda
piomar123 <piomar123@gmail.com>
2016-10-24 19:42:45 +0200

    Added default digest to md5

--- actions/digest.js
+++ actions/digest.js
@@ -1,23 +1,17 @@
 var os = require("os");
 var helpers = require("../helpers");
 
-
-
 var task =  function(request, callback){
-
-	var algorithms = request.body.alg ? Object.keys(request.body.alg) : [];
+	var algorithms = request.body.alg ? Object.keys(request.body.alg) : ['md5'];
 	var loopCount = request.body.loop ? request.body.loop : 1;
 	var doc = request.body.txt ? request.body.txt : "";
-	
-	
-	helpers.calculateMultiDigest(doc, 
-		algorithms, 
-		function(err, digests) {
-			callback(null, digests.join("<br>") + ("<hr>  <br>Service provided by: " + os.hostname()));	
-		}, 
-		loopCount);
 
-	
+	helpers.calculateMultiDigest(doc,
+		algorithms,
+		function(err, digests) {
+			callback(null, digests.join("<br>") + ("<hr>  <br>Service provided by: " + os.hostname()));
+		},
+		loopCount);
 }
 
 exports.action = task
